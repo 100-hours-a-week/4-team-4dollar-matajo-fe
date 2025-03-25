71d9dbdcde61c30806297701804ea812f87f62c5
gerard.park <dev.gerardpark@gmail.com>
2025-03-25 16:49:15 +0900

    fix: 깃헙 액션3

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
+test('메인 레이아웃이 정상적으로 렌더링되는지 확인', () => {
   render(
     <BrowserRouter>
       <App />
     </BrowserRouter>,
   );
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // MainRedirect 컴포넌트로 인해 /main으로 리다이렉트됨
+  expect(window.location.pathname).toBe('/main');
 });
