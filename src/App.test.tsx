e8d05368fba90dcfa784efeebfdfdf07278b03cf
gerard.park <dev.gerardpark@gmail.com>
2025-03-26 02:34:16 +0900

    fix: 깃헙액션 테스트파일 추가 및 수정

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,13 +3,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-test('메인 레이아웃이 정상적으로 렌더링되는지 확인', () => {
+test('비로그인 상태에서 로그인 페이지로 리다이렉트되는지 확인', () => {
   render(
     <BrowserRouter>
       <App />
     </BrowserRouter>,
   );
 
-  // MainRedirect 컴포넌트로 인해 /main으로 리다이렉트됨
-  expect(window.location.pathname).toBe('/main');
+  // 로그인하지 않은 상태이므로 /login으로 리다이렉트됨
+  expect(window.location.pathname).toBe('/login');
 });
