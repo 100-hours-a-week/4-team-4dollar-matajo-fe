81cff4a8d4b238ae8e8ae145e8f91bbcec54d0ef
yujinjeong0917 <yujin010917@khu.ac.kr>
2025-03-27 14:57:27 +0900

    fix: Sentry BrowserTracing 오류 수정

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,20 @@ import App from './App';
 import './index.css';
 import { AuthProvider } from './contexts/auth';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: 'https://o4509048071847936.ingest.us.sentry.io/4509048076107776',
+  integrations: [Sentry.browserTracingIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [
+    'localhost',
+    /^https:\/\/matajo\.store\/api/,
+    /^https:\/\/matajo\.store/,
+  ],
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
@@ -18,7 +32,4 @@ root.render(
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
