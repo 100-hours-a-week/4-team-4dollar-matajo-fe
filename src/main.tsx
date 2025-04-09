d18d8c0a9af4843246687002c5314ee82bc8de1f
yujinjeong0917 <yujin010917@khu.ac.kr>
2025-04-09 16:48:16 +0900

    refactor: 스타일 파일 구조 정리
    - Tailwind CSS 관련 코드 제거
    - index.css와 styles/index.css를 global.css로 통합
    - styled-components theme.ts 유지
    - 기본 스타일 설정 정리

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './styles/index.css';
+import './styles/global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './contexts/auth';
